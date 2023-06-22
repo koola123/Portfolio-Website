9d9b44366f037f6dc5f2b38a6679620dccc0d93b
Matthias Müringer <mmueringer@web.de>
2023-06-22 22:55:36 +0200

    add dark theme to the website

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
+const darkButton = document.getElementById("dark-btn");
+const btnText = document.getElementById("btnText");
+// btnText.style.color = "#cccc00";
+btnText.style.fontSize ="1.1rem";
+const btnIcon = document.getElementById("btnIcon");
+
+  darkButton.addEventListener("click", toggleThemeColor);
+
+  function toggleThemeColor() {
+    document.body.classList.toggle("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")) {
+      btnIcon.src = "images/sun_icon.svg";
+      btnText.textContent= "Light";
+    } else {
+      btnIcon.src = "images/moon_icon.svg";
+      btnText.textContent = "Dark";
+    }
+  };
+
 window.addEventListener("scroll", function() {
   const nav = document.querySelector("nav");
   nav.classList.toggle("sticky", window.scrollY > 550);
-})
+});
 
 function opentab(tabname) {
   let tablinks = document.querySelectorAll(".tab-links");
@@ -14,7 +34,7 @@ function opentab(tabname) {
   }
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
-}
+};
 
 const sideMenu = document.getElementById("sidemenu");
 const closeButton = document.querySelector(".close-button");
@@ -26,12 +46,12 @@ closeButton.addEventListener("click", toggleMenu);
 function toggleMenu() {
   let sideMenu = document.getElementById("sidemenu");
   sideMenu.classList.toggle("show");
-}
+};
 
 function hideMenu() {
   let sideMenu = document.getElementById("sidemenu");
   sideMenu.classList.remove("show");
-}
+};
 
 sideMenu.addEventListener("click", (e) => {
   let target = e.target;
@@ -65,4 +85,4 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 4000);
-}
+};
